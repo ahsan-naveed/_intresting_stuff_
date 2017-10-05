65121a455651154b41b0ca03ea2a25885a5d5578
Ahsan Naveed <ahsannaveed007@yahoo.com>
2017-10-05 13:51:26 -0700

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Palindrome {
+	public static void main (String[] args) {
+		System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+		System.out.println(isPalindrome("panama"));
+	}
+
+	public static boolean isPalindrome(String str) {
+		int i = 0, j = str.length() - 1; 
+		while(i<j) {
+			while (i<j && !Character.isLetterOrDigit(str.charAt(i))) i++;
+			while (i<j && !Character.isLetterOrDigit(str.charAt(j))) j--;
+			if(Character.toLowerCase(str.charAt(i)) 
+				!= Character.toLowerCase(str.charAt(j))) return false;
+			i++;
+			j--;
+		}
+		return true;
+	} 
+}

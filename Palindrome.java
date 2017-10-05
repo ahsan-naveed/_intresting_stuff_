94cc9ade186571ae272d27ab750a788e244348d3
Ahsan Naveed <ahsannaveed007@yahoo.com>
2017-10-05 13:52:45 -0700

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -9,7 +9,9 @@ public class Palindrome {
 	public static boolean isPalindrome(String str) {
 		int i = 0, j = str.length() - 1; 
 		while(i<j) {
+			// skip non-alphanumeric character from head
 			while (i<j && !Character.isLetterOrDigit(str.charAt(i))) i++;
+			// skip non-alphanumeric character form tail
 			while (i<j && !Character.isLetterOrDigit(str.charAt(j))) j--;
 			if(Character.toLowerCase(str.charAt(i)) 
 				!= Character.toLowerCase(str.charAt(j))) return false;

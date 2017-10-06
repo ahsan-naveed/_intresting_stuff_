ac17b990054117cb80afd522ddf110f778f6fe8f
Ahsan Naveed <ahsannaveed007@yahoo.com>
2017-10-06 00:34:03 -0700

    Create ReverseStringII.java

---
+++ ReverseStringII.java
@@ -0,0 +1,21 @@
+// in-place reversal
+
+import java.util.*;
+
+public class ReverseStringII {
+	public static void main (String[] args) {
+		StringBuilder str = new StringBuilder("the sky is blue");
+		System.out.println(reverse(new StringBuilder(reverse(str))));
+	}
+	public static String reverse(StringBuilder str) {
+		int i = 0, j = str.length() - 1;
+		while (i != j) {
+			char temp = str.charAt(i);
+			str.setCharAt(i, str.charAt(j));
+			str.setCharAt(j, temp);
+			i++;
+			j--;
+		}
+		return new String(str);
+	}
+}

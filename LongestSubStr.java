0d70476e69b81a56e4b0c9291ed2d963e420acf9
Ahsan Naveed <ahsannaveed007@yahoo.com>
2017-10-09 23:58:51 -0700

    Update LongestSubStr.java

--- LongestSubStr.java
+++ LongestSubStr.java
@@ -3,16 +3,34 @@ import java.util.*;
 public class LongestSubStr {
 	public static void main (String[] args) {
 		// Testcase 1 : should return 1
-		String s = "bbbbb";
-		System.out.println(longest(s));
+		String s = "bbbbbbabcdefghbbbbabcdef";
+		System.out.println(longest(lean(s)));
 	}
 	public static int longest (String s) {
 		Map<Character, Integer> map = new HashMap<>();
+		ArrayList<Integer> list = new ArrayList<>();
 		for (int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
-			if (map.containsKey(c)) return map.size();
+			if (map.containsKey(c)) {
+				list.add(map.size());
+				map.clear();	
+			}
 			else map.put(c, 1);
 		}
-		return 0;
+		return list.size() > 0 ? Collections.max(list) : 0;
 	}
+
+	// remove repeated chars 
+	public static String lean(String s) {
+		StringBuilder str = new StringBuilder();
+		for (int i = 0; i < s.length() - 1; i++ ) {
+			char c1 = s.charAt(i);
+			char c2 = s.charAt(i+1);
+			if (c1 != c2) str.append(c1);	
+		}
+		// get the last char
+		str.append(s.charAt(s.length() -1));
+		return new String(str);	
+	}
+
 }
